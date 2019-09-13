62021434c6d9eda96f299a75eff416bbf33b975e
Ady Gould <adrian.gould@nmtafe.wa.edu.au>
2019-09-13 10:52:31 +0800

    Demo branch mini change

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>This is a demo</h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
